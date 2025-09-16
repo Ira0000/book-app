6e8ac04332e0b54e080ff02a87e9aebb65e7d499
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2025-09-16 15:50:57 +0200

    modify Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-export default function Home({ children }: { children: React.ReactNode }) {
+export default function Home() {
   return (
-    <div className="p-5 md:p-8 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full">{children}</main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-    </div>
+    <div className="p-5 md:p-8 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]"></div>
   );
 }
